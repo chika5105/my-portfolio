5b55cf42f58e84f7d234c8b447779b775e08be23
Chika-Jinanwa <46027071+Chika-Jinanwa@users.noreply.github.com>
2020-08-05 01:57:56 -0500

    removed whitenoise static storage

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -148,7 +148,7 @@ EMAIL_HOST_PASSWORD = 'thisismypassword123'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 django_heroku.settings(locals())
